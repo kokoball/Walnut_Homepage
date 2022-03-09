680e5dcdbff260965b228ac6c31c4e7f6c9f7a73
kokoball <kbp4154@naver.com>
2022-03-09 13:20:25 +0900

    :lipstick:  UI: golbalStyle background color 수정
    Closes #18

--- styles/globalStyle.ts
+++ styles/globalStyle.ts
@@ -3,22 +3,6 @@ import { reset } from 'styled-reset';
 import { theme } from 'styles';
 
 const globalStyle = createGlobalStyle`
-		@font-face {
-		font-family: 'Apple SD Gothic Neo';
-		font-style: normal;
-		font-weight: 400;
-		src: local('AppleSDGothicNeoM00'),
-			url('../public/fonts/AppleSDGothicNeo/AppleSDGothicNeoM.ttf')
-				format('truetype'),
-			url('../public/fonts/AppleSDGothicNeo/AppleSDGothicNeoM.eot')
-				format('embedded-opentype'),
-			url('../public/fonts/AppleSDGothicNeo/AppleSDGothicNeoM.woff2')
-				format('woff2'),
-			url('../public/fonts/AppleSDGothicNeo/AppleSDGothicNeoM.woff')
-				format('woff');
-		unicode-range: U+AC00- U+D7A3, U+0030-0039;
-	}
-
 	${reset}
 	*, *::before, *::after {
     box-sizing: border-box;
@@ -28,7 +12,6 @@ const globalStyle = createGlobalStyle`
 		margin: 0;
 	}
 	html, body {
-		background-color: #fff;
 		font-family: 'Apple SD Gothic Neo',system-ui, Dotum,'돋움', Helvetica, sans-serif;
 	}
 	body {
