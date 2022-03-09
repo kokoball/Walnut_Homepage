5472080353ef62abe0fe469b754bf822a6329a62
kokoball <kbp4154@naver.com>
2022-03-09 16:13:53 +0900

    :pencil:  Docs: meta 정보 입력

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,7 +32,12 @@ class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
-          <meta charSet="utf-8" />
+          <meta name="title" content="땅콩스쿨, 가장 가까운 문해력 유치원" />
+          <meta
+            name="description"
+            content="3만원으로 즐기는 매달 새로운 32권의 독서 커리큘럼. 평일 4~9시 캐릭터 선생님과 실시간 랜선라이브에 참여하세요!"
+          ></meta>
+          <meta property="og:title" content="땅콩스쿨"></meta>
         </Head>
         <body>
           <Main />
